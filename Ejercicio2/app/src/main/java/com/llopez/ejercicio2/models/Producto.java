ea2635b1055db5639e2ac0bf1eab6fe91f5a8297
Leonardo López <leonardo@Leonardos-MacBook-Air.local>
2022-05-07 21:51:49 -0600

    ejercicio 2

---
+++ Ejercicio2/app/src/main/java/com/llopez/ejercicio2/models/Producto.java
@@ -0,0 +1,26 @@
+package com.llopez.ejercicio2.models;
+
+public class Producto {
+    private String codigo;
+    private String descripcion;
+    private float precio;
+
+
+    public Producto(String codigo, String descripcion, float precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+}
---
+++ Ejercicio2/app/src/main/java/com/llopez/ejercicio2/models/Respuesta.java
@@ -0,0 +1,14 @@
+package com.llopez.ejercicio2.models;
+
+public class Respuesta {
+    private boolean ok;
+    private int resultado;
+
+    public boolean getOk() {
+        return ok;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+}
